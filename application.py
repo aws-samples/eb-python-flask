7310748a703ffae2600dd293a45da24c91bdf1fc
Evan Brown <evandbrown@gmail.com>
2013-07-11 21:33:14 -0700

    Use env vars for feature flags and Flask debugging

--- application.py
+++ application.py
@@ -1,15 +1,25 @@
 import flask
+import os
  
 application = flask.Flask(__name__)
 
-#Set application.debug=true to enable tracebacks on Beanstalk log output. 
-#Make sure to remove this line before deploying to production.
-application.debug=True
- 
+# Only enable Flask debugging if an env var is set to true
+application.debug = os.environ.get('FLASK_DEBUG') in ['true', 'True']
+
+# Get application version from env
+app_version = os.environ.get('APP_VERSION')
+
+# Get cool new feature flag from env
+enable_cool_new_feature = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 'True']
+
 @application.route('/')
 def hello_world():
     message = "Hello, world!"
-    return flask.render_template('index.html', title=message)
+    return flask.render_template('index.html',
+                                  title=message,
+                                  flask_debug=application.debug,
+                                  app_version=app_version,
+                                  enable_cool_new_feature=enable_cool_new_feature)
  
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0')
